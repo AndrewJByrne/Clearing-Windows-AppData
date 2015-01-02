318b598592fd376fdffbe314d158f5bbf7b919b5
Andrew Byrne <andrew.j.byrne@hotmail.com>
2015-01-02 00:20:29 -0800

    Added status messages.

--- DebugSettings/DebugFlyout.xaml.cs
+++ DebugSettings/DebugFlyout.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -30,30 +31,36 @@ namespace DebugSettings
 
         private async void ClearData_Click(object sender, RoutedEventArgs e)
         {
+            StringBuilder status = new StringBuilder();
             if (ClearLocal.IsChecked.HasValue && ClearLocal.IsChecked.Value)
             {
                 Debug.WriteLine("Clearing local app data");
                 await ApplicationData.Current.ClearAsync(ApplicationDataLocality.Local);
-
+                status.AppendLine("Cleared local app data");
             }
 
             if (ClearRoaming.IsChecked.HasValue && ClearRoaming.IsChecked.Value)
             {
                 Debug.WriteLine("Clearing roaming app data");
                 await ApplicationData.Current.ClearAsync(ApplicationDataLocality.Roaming);
+                status.AppendLine("Cleared roaming app data");
             }
 
             if (ClearTemporary.IsChecked.HasValue && ClearTemporary.IsChecked.Value)
             {
                 Debug.WriteLine("Clearing temporary app data");
                 await ApplicationData.Current.ClearAsync(ApplicationDataLocality.Temporary);
+                status.AppendLine("Cleared temp app data");
             }
 
             if (ClearLocalCache.IsChecked.HasValue && ClearLocalCache.IsChecked.Value)
             {
                 Debug.WriteLine("Clearing local cache app data");
                 await ApplicationData.Current.ClearAsync(ApplicationDataLocality.LocalCache);
+                status.AppendLine("Cleared local cache app data");
             }
+
+            LastStatus.Text = status.ToString();
         }
 
         private void CopyLocal_Click(object sender, RoutedEventArgs e)
@@ -61,6 +68,7 @@ namespace DebugSettings
             DataPackage dp = new DataPackage();
             dp.SetText(Windows.Storage.ApplicationData.Current.LocalFolder.Path);
             Clipboard.SetContent(dp);
+            LastStatus.Text = "Copied local folder path to clipboard.\nPaste into Windows Explorer to browse that folder.";
         }
 
         private void CopyLocalCache_Click(object sender, RoutedEventArgs e)
@@ -71,6 +79,7 @@ namespace DebugSettings
             // I have to create the local cache path based on its relative location to all other folders myself. 
             dp.SetText(Path.Combine(Path.GetDirectoryName(Windows.Storage.ApplicationData.Current.RoamingFolder.Path),"LocalCache"));
             Clipboard.SetContent(dp);
+            LastStatus.Text = "Copied local cache folder path to clipboard.\nPaste into Windows Explorer to browse that folder.";
         }
 
         private void CopyTemporary_Click(object sender, RoutedEventArgs e)
@@ -78,6 +87,7 @@ namespace DebugSettings
             DataPackage dp = new DataPackage();
             dp.SetText(Windows.Storage.ApplicationData.Current.TemporaryFolder.Path);
             Clipboard.SetContent(dp);
+            LastStatus.Text = "Copied temp folder path to clipboard.\nPaste into Windows Explorer to browse that folder.";
         }
 
         private void CopyRoaming_Click(object sender, RoutedEventArgs e)
@@ -85,6 +95,7 @@ namespace DebugSettings
             DataPackage dp = new DataPackage();
             dp.SetText(Windows.Storage.ApplicationData.Current.RoamingFolder.Path);
             Clipboard.SetContent(dp);
+            LastStatus.Text = "Copied roaming folder path to clipboard.\nPaste into Windows Explorer to browse that folder.";
         }
     }
 }
